726ebca04aa0811722fd2840302a7a83ad3de8c6
DuyTheCoGa <47093479+zamminhduy123@users.noreply.github.com>
2021-12-03 09:27:42 +0700

    Set up folder and project structure
    - Package download:
    "cookie-parser": "^1.4.6",
    "express-handlebars": "^6.0.2",
    "express-session": "^1.17.2",
    "pg-promise": "^10.11.1",
     "sha.js": "^2.4.11"
    
    - Set up server on port 3000
    - Adding views and layouts
    - Adding template code inside

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const port = 3000;
+const app = express();
+
+const hbs = exphbs.create({
+  defaultLayout: "mainLayout",
+  extname: "hbs",
+});
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    cssP: () => "css",
+    scriptP: () => "scripts",
+    navP: () => "nav",
+    footerP: () => "footer",
+    title: "Home"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on localhost:${port}`);
+});
