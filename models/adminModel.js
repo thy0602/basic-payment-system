1c2f778298bae7261f1d5c9850c59f7c2da61cec
zamminhduy123 <ntminhduy123@gmail.com>
2021-12-26 23:42:59 +0700

    Update adminModel.js

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,10 +1,23 @@
 const db = require("../db/db");
 const tableName = "admin";
-const PKFieldName = "username", pwdFieldName = "password", balFieldName = "balance";
+const tableFields = {
+    username: 'username',   // Primary Key
+    balance: 'balance',
+    password: 'password'
+}
 
 exports.getAll = async () => {
     const res = await db.getAll(tableName);
-    return res[0];
+    return res;
 }
 
-
+exports.getByUsername = async (username) => {
+    const res = await db.getByAField(tableName, PKFieldName, username);
+    return res;
+}
+exports.getOne = async () => {
+    const res = await db.getAll(tableName);
+    if (res.length > 0)
+        return res[0];
+    return null;
+}
