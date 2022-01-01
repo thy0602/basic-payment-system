0af6e286c80b6c46e96a63988a900f6ee6cd8738
thy0602 <pnmthy0602@gmail.com>
2022-01-01 14:39:04 +0700

    Change password postgres

--- db/db_config.js
+++ db/db_config.js
@@ -6,7 +6,7 @@ const connection = {
     user: 'postgres',
     host: 'localhost',
     database: 'payment_system',
-    password: 'Huunhan0811',
+    password: 'postgres',
     port: 5432,
     max: 30,
 };
