4920a474b375430fbe202023ef99001855f062a4
thy0602 <pnmthy0602@gmail.com>
2022-01-19 11:44:07 +0700

    Update payment api

--- api/paymentApi.js
+++ api/paymentApi.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const salt = 10;
+const jwt = require('jsonwebtoken');
+const secretKey = 'ThisIsASecretKey';
 
 const accountModel = require("../models/accountModel");
 const TransactionManager = require("../TransactionManager/TransactionManager");
@@ -27,9 +29,26 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { username, password, amount } = req.body;
+  const { username, token, amount } = req.body;
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // getInTransactions()
+  // let token = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDMxMjI5NTg0NzcsInVzZXJuYW1lIjoiSURfMDAxIn0.h96IrzcDv_fOpSu5g14_JI4Ps4Jkn6s701pr1n7iM90";
+  let is_err = false;
+  jwt.verify(token, secretKey, function(err, decoded) {
+      if (err) {
+        console.log("Error 1:", err.message);
+        is_err = true;
+        return res.status(400).send({ error: "Unauthorized", message: "Unauthorized" });
+      }
+
+      console.log("Decoded: ", decoded);
+      if (decoded.username != username) {
+        is_err = true;
+        return res.status(400).send({ error: "Unauthorized", message: "Invalid user" });
+      }
+  });
+  if (is_err)
+    return;
 
   let account = await accountModel.getByUsername(username);
 
@@ -38,36 +57,31 @@ router.post("/", async (req, res) => {
     return res.status(401).send("Invalid username");
   }
   if (account.balance < amount) {
-    res.status(401).send("Insufficient Balance!");
+    return res.status(422).send("Insufficient Balance!");
   }
 
-  const isUser = await bcrypt.compareSync(password, account.password);
-  if (isUser) {
-    //proceed payment
-    const time = new Date();
-    const transaction = {
-      amount: amount,
-      created_at: time.toISOString(),
-      type: 1, //pay type
-      username: username,
-      status: "P", //pending transact
-    };
-    account.balance = Number(account.balance) - Number(amount);
+  //proceed payment
+  const time = new Date();
+  const transaction = {
+    amount: amount,
+    created_at: time.toISOString(),
+    type: 1, //pay type
+    username: username,
+    status: "P", //pending transact
+  };
+  account.balance = Number(account.balance) - Number(amount);
 
-    const createdTransac = await transactionRecordModel.createTransaction(
-      transaction,
-      account
-    );
+  const createdTransac = await transactionRecordModel.createTransaction(
+    transaction,
+    account
+  );
 
-    if (createdTransac.isCreated) {
-      const transactionManager = TransactionManager;
-      transactionManager.add(createdTransac.data);
-      res.status(200).send({ message: "Transaction Completed!" });
-    } else {
-      res.status(401).send({ error: "Error while creating transaction!", message: createdTransac.data });
-    }
+  if (createdTransac.isCreated) {
+    const transactionManager = TransactionManager;
+    transactionManager.add(createdTransac.data);
+    res.status(200).send({ message: "Transaction Completed!" });
   } else {
-    res.status(401).send({ error: "Password Incorrect!" });
+    res.status(422).send({ error: "Error while creating transaction!", message: createdTransac.data });
   }
 });
 
